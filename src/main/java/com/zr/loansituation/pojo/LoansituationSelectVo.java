811e45afac56bdea46ae7ccd43d16d3be2e4a87d
star <2043327900@qq.com>
2019-06-22 15:17:17 +0800

    大猩猩

--- src/main/java/com/zr/loansituation/pojo/LoansituationSelectVo.java
+++ src/main/java/com/zr/loansituation/pojo/LoansituationSelectVo.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.zr.util.PageVo;
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * Created by star on 2019/6/22.
  */
@@ -11,51 +13,50 @@ import lombok.Data;
 public class LoansituationSelectVo extends PageVo {
 
 //    private  Integer id;
-
     private String idCode;      //身份证号码
 
     private String customerName;   //客户姓名
     private Integer contactNumber;//联系电话
 
+   private Integer loanFishStatus; //贷鱼状态
+    private Integer pushStatus; //推送状态
+    private Integer signatureStatus;    //签章状态
+
     private String merchant;    //商户
 
- /*   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
-    private Data incomingPartsTime; //进件时间
+
+   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
+    private Date incomingPartsTime; //进件时间
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
-    private Data incomingPartsTimeStart; //进件时间(开始)
+    private Date incomingPartsTimeStart; //进件时间(开始)
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
-    private Data incomingPartsTimeEnd; //进件时间(结束)
+    private Date incomingPartsTimeEnd; //进件时间(结束)
+
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
-    private  Data letterTrialTime;  //信审日期
+    private Date letterTrialTime;  //信审日期
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
-    private  Data letterTrialTimeStart;  //信审日期(开始)
+    private  Date letterTrialTimeStart;  //信审日期(开始)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
-    private  Data letterTrialTimeEnd;  //信审日期(结束)
+    private  Date letterTrialTimeEnd;  //信审日期(结束)
+
 
     private String managementNum;   //资方申请编号
 
     private Integer incomingPartsChannel;   //进件渠道
 
-
-
-
     private  Integer term;  //期限
-
-    private Data approvalTime;  //审批时间
-
-
-    private  Data createTime;   //创建时间
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
+    private Date approvalTime;  //审批时间
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
+    private  Date createTime;   //创建时间
 
     private  String createName;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
+    private Date updateTime;
 
-    private Data updateTime;
-
-    private String updateName;*/
-
-
-
+    private String updateName;
 
 }
