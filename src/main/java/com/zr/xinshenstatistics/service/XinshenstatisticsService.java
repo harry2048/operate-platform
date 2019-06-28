f0dbd8b168a13665673a9e4a061217ca05490243
pingping <3566602019@qq.com>
2019-06-28 10:39:46 +0800

    江萍

---
+++ src/main/java/com/zr/xinshenstatistics/service/XinshenstatisticsService.java
@@ -0,0 +1,20 @@
+package com.zr.xinshenstatistics.service;
+
+
+import com.zr.xinshenstatistics.pojo.XinshenstatisticsVO;
+import com.zr.xinshenstatistics.util.AllRecords;
+import com.zr.xinshenstatistics.util.ResultVo;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by 86151 on 2019/6/14.
+ */
+public interface XinshenstatisticsService {
+
+    ResultVo<AllRecords> queryPage(XinshenstatisticsVO xinshenstatisticsVO);
+
+    ResultVo exportExcel(HttpServletResponse resp, XinshenstatisticsVO xinshenstatisticsVO)throws Exception;
+
+    ResultVo queryById(Integer id);
+}
