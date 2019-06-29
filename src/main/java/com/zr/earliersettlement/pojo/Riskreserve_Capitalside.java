d107f80be778d2b5eb43832a8dc4269c2d72a14c
xhy11 <1170305960@qq.com>
2019-06-29 11:37:50 +0800

    许浩洋

---
+++ src/main/java/com/zr/earliersettlement/pojo/Riskreserve_Capitalside.java
@@ -0,0 +1,14 @@
+package com.zr.earliersettlement.pojo;
+
+import lombok.Data;
+
+/**
+ * Created by star on 2019/6/29.
+ */
+@Data
+public class Riskreserve_Capitalside {
+
+    private  Integer id;
+
+    private  String capitalside;
+}
--- src/main/java/com/zr/loansituation/service/LoansituationService.java
+++ src/main/java/com/zr/loansituation/service/LoansituationService.java
@@ -17,6 +17,5 @@ public interface LoansituationService {
 
     ResultVO queryAll();
 
-    ResultVO queryAll2();
 
 }
